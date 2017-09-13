7b6c78276489db9b0321abeeab4e0ef9f37e0e46
m85091081 <m85091081@gmail.com>
2017-09-13 02:07:05 +0000

    modify mongo

--- conf/dbconf.py
+++
@@ -1,3 +0,0 @@
-db_addr = 'mongodb://192.168.0.64:27017/'
-db_user = ''
-db_pass = ''
--- module/mongo.py
+++ module/mongo.py
@@ -1,4 +1,3 @@
 from pymongo import MongoClient
-from config import dbconf
 
-client = MongoClient(dbconf.db_addr,user = dbconf.db_user,password = dbconf.db_pass, authSource='admin',authMechanism='SCRAM-SHA-1')
+client = MongoClient('mongodb://:@/admin?authMechanism=SCRAM-SHA-1')
