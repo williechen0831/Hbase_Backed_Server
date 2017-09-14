65e1e1d68cd55106f3abbe52c370779a90a3886e
m85091081 <m85091081@gmail.com>
2017-09-14 21:40:50 +0800

    fix api

--- view/api.py
+++ view/api.py
@@ -8,4 +8,4 @@ api = Blueprint('api')
 @api.route('/car/<car>')
 async def findCar(request,car):
     dbCar = posData.getlastcar(int(car))
-    return json({'car':dbCar.get('car'),'X':dbCar.get('X'),'Y':dbCar.get('Y'),'V':dbCar.get('V'),'time':dbCar.get('time')})
+    return json({'car':dbCar['car'],'X':dbCar['X'],'Y':dbCar['Y'],'V':dbCar['V'],'time':dbCar['time']})
