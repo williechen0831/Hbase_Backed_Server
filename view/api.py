f4c311951e84cc6e14408300ff79bfb38490e0d5
m85091081 <m85091081@gmail.com>
2017-09-14 22:08:09 +0800

    add try error

--- view/api.py
+++ view/api.py
@@ -7,5 +7,8 @@ api = Blueprint('api')
 
 @api.route('/car/<car>')
 async def findCar(request,car):
-    dbCar = posData.getlastcar(int(car))[0]
+    try:
+        dbCar = posData.getlastcar(int(car))[0]
+    except:
+        return json({'message':'dont have this car'})
     return json({'car':dbCar.get('car'),'X':dbCar.get('X'),'Y':dbCar.get('Y'),'V':dbCar.get('V'),'time':dbCar.get('time')})
