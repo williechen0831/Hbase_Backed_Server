ae6974d70d4c15aeb73fab220566135776c8d898
m85091081 <m85091081@gmail.com>
2017-09-19 22:08:00 +0800

    try error

--- module/mongo.py
+++ module/mongo.py
@@ -30,7 +30,7 @@ class PosData:
         cars = {}
         for car in range(0,7):
             carNeed = self.col.find({car : car , time : {"gte":seclater,"lte":datetimeNow }}).sort('_id',-1).limit(1)
-            print(carNeed)
+            print(carNeed[0])
             try:
                 cars[car] = [carNeed[0].get('X'),carNeed[0].get('Y'),carNeed[0].get('X')]
             except:
