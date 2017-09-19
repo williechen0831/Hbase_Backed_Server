d01a39e4a66a8fe9f3967203dc6a1f13fc69cdd0
williechen0831 <chenjw0831@gmail.com>
2017-09-19 20:34:50 +0800

    modify compute

--- module/compute.py
+++ module/compute.py
@@ -9,7 +9,7 @@ def compass(pointA, pointB):
     x = math.sin(diffLong) * math.cos(lat2)
     y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
             * math.cos(lat2) * math.cos(diffLong))
-
+         
     initial_bearing = math.atan2(x, y)
     initial_bearing = math.degrees(initial_bearing)
     compass_bearing = (initial_bearing + 360) % 360
@@ -19,5 +19,33 @@ def compass(pointA, pointB):
 def distance(pointA,pointB):
     import geopy.distance
     return geopy.distance.vincenty(pointA, pointB).km
+
+def determine(angle):
+    return abs(1.875/math.cos(angle))
+
+def block(angle,d):
+    if angle>0 and angle<90:
+        if d < determine(angle):
+            return 2
+        else:
+            return 3
+    if angle>90 and angle <180:
+        if d < determine(angle):
+            return 2
+        else:
+            return 1
+    if angle>180 and angle <270:
+        if d < determine(angle):
+            return 5
+        else:
+            return 4
+    if angle>270 and angle<360:
+        if d < determine(angle):
+            return 5
+        else:
+            return 6
+ 
+def resultans():
+    from module.mongo import PosData
 
 
