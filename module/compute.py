ab9c1df893a4577ce0cbb8ab71b59b176f5b6b6e
m85091081 <m85091081@gmail.com>
2017-09-20 08:36:54 +0800

    fix dis

--- module/compute.py
+++ module/compute.py
@@ -16,8 +16,19 @@ def compass(pointA, pointB):
     compass_bearing = (initial_bearing + 360) % 360
     return compass_bearing
 
-def distance(pointA,pointB):
-    return geopy.distance.vincenty(pointA, pointB).km
+def distance(origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+return d
 
 def determine(angle):
     return abs(1.875/math.cos(angle))
