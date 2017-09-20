93445afbfaad169b44296a52e4cc6b57c309290d
m85091081 <m85091081@gmail.com>
2017-09-20 09:58:07 +0800

    fix compute

--- module/compute.py
+++ module/compute.py
@@ -5,10 +5,13 @@ from conf.dask_conf import client
 import shared
 
 def block_check(rawList):
-    six_list = []
-    for x in range(0,5):
-        six_list.append((float(rawList[0].get('X')),float(rawList[0].get('Y'))))
-    listcompass = client.gather(client.map(shared.compass,six_list,(float(rawList[range(1,7)].get('X')),float(rawList[range(1,7)].get('Y')))))
+    six_raw_list = []
+    six_other_list = []
+    for x in range(1,7):
+        six_raw_list.append((float(rawList[0].get('X')),float(rawList[0].get('Y'))))
+        six_other_list.append((float(rawList[x].get('X')),float(rawList[x].get('Y'))))
+    listcompass = client.gather(client.map(shared.compass,six_raw_list,six_other_list))
+    listdis = client.gather(client.map(shared.distance,six_raw_list,six_other_list))
     return listcompass
 
 def block_check_1(rawList):
