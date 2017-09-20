ee82744412e27cb5669491270488d649e1ff5c3b
m85091081 <m85091081@gmail.com>
2017-09-20 10:11:53 +0800

    fix compute

--- module/compute.py
+++ module/compute.py
@@ -10,26 +10,11 @@ def block_check(rawList):
     for x in range(1,7):
         six_raw_list.append((float(rawList[0].get('X')),float(rawList[0].get('Y'))))
         six_other_list.append((float(rawList[x].get('X')),float(rawList[x].get('Y'))))
-    listcompass = client.gather(client.map(shared.compass,six_raw_list,six_other_list))
-    listdis = client.gather(client.map(shared.distance,six_raw_list,six_other_list))
-    return listcompass
+    listCompass = client.gather(client.map(shared.compass,six_raw_list,six_other_list))
+    listDis = client.gather(client.map(shared.distance,six_raw_list,six_other_list))
+    listBlock = client.gather(client.map(shared.block,listCompass,listDis))
+    return listBlock
 
-def block_check_1(rawList):
-    listcompass = []
-    listdis = []
-    listblock = []
-    for x in rawList:
-        listdis.append(
-                distance((float(rawList[0].get('X')),float(rawList[0].get('Y'))),(float(x.get('X')),float(x.get('Y'))))*1000)
-        listcompass.append(
-                compass((float(rawList[0].get('X')),float(rawList[0].get('Y'))),(float(x.get('X')),float(x.get('Y')))))
-    u = 0
-    for x in listcompass:
-        listblock.append(block(x,listdis[u]))
-        u = u + 1
-
-
-    return listblock
 
 def getrecentcar_parallel():
     carsObj = client.gather(client.map(shared.gettrangecar,range(7)))
