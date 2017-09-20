b96753e769f705adbefb72ea4cecf3fe216b6ece
m85091081 <m85091081@gmail.com>
2017-09-20 10:48:43 +0800

    fix compute

--- module/compute.py
+++ module/compute.py
@@ -5,8 +5,15 @@ def block_check(rawList):
     six_raw_list = []
     six_other_list = []
     for x in range(1,7):
-        six_raw_list.append((float(rawList[0].get('X')),float(rawList[0].get('Y'))))
-        six_other_list.append((float(rawList[x].get('X')),float(rawList[x].get('Y'))))
+        try:
+            six_raw_list.append((float(rawList[0].get('X')),float(rawList[0].get('Y'))))
+        except:
+            six_raw_list.append((0,0))
+        try:
+            six_other_list.append((float(rawList[x].get('X')),float(rawList[x].get('Y'))))
+        except:
+            six_other_list.append((0,0))
+
     listCompass = client.gather(client.map(shared.compass,six_raw_list,six_other_list))
     listDis = client.gather(client.map(shared.distance,six_raw_list,six_other_list))
     listBlock = client.gather(client.map(shared.block,listCompass,listDis))
